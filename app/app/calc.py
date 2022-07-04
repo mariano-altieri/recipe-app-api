6b83f7dd9ce04f8b8d8d39cf8da0073878070577
Mariano Altieri <marianoa@givingassistant.org>
2022-07-04 20:28:28 -0300

    Configured docker compose and checks to wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and returns result"""
+    return x + y
+
+
+def substract(x, y):
+    """Substract x from y and returns result"""
+    return y - x
