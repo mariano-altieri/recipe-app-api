46223a341da5824e8c828a9e815c775485104e6b
Mariano Altieri <marianoa@givingassistant.org>
2022-07-05 22:17:37 -0300

    Add Ingredients and Tags APIs

---
+++ app/recipe/views.py
@@ -0,0 +1,64 @@
+"""
+Views for Recipes API
+"""
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Recipe, Tag, Ingredient
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for managing recipe APIs."""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Returns the serializer class for request"""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
+
+
+class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
+                            mixins.UpdateModelMixin,
+                            mixins.ListModelMixin,
+                            viewsets.GenericViewSet):
+    """Base ViewSet for Recipe Attributes"""
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """View for managing tag APIs."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+
+    def get_serializer_class(self):
+        """Returns the serializer class for request"""
+        if self.action == 'list':
+            return serializers.TagSerializer
+
+        return self.serializer_class
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """View for managing ingredients APIs."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
